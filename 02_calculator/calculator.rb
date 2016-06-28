d7fe1ceadd39cc20814045f36be0d21022296eaa
gscheinman <gscheinman@Griffins-MacBook-Pro.local>
2016-06-28 14:26:18 -0700

    here it is

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
-#write your code here
+def add (num1, num2)
+    num1+num2
+end
+
+def subtract(num3, num4)
+    num3 - num4
+end
+
+def sum (arr)
+    total = 0
+    arr.each {|num|
+        total = total + num}
+    total
+    end
+
+
+    
